b870d3e457e96f8295ae0d945e6a92b1f34f067d
Emily Chimbo <emily.chimbo@grupobusiness.it>
2026-01-22 16:17:00 -0500

    other test

--- BitPromptStudioBackend/Program.cs
+++ BitPromptStudioBackend/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. .
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
